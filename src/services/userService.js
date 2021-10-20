d701add8bb8119fc1d65b729466999f8bd5fc27d
McBobson <maciej.balcer.98@gmail.com>
2021-10-20 22:50:33 +0200

    Pierwszy zarys tabeli

--- src/services/userService.js
+++ src/services/userService.js
@@ -4,4 +4,7 @@ export default {
     getUser(id) {
         return api.get(`user/${id}`).then((response) => response.data);
     },
+    fetchUsers() {
+        return api.get("user/").then((response) => response.data);
+    },
 };
